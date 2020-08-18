1c22c2d6e8d011b3636a1f745225509614bdad96
Cory Marker <cmarker4@gmail.com>
2020-08-18 16:52:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,8 +9,8 @@ class Song
 
   def initialize(name, artist = nil)
     @name = name
-    @artist = artist
-    @@all << self
+    # @artist = artist
+    # @@all << self
   end
 
   def artist_name
@@ -20,4 +20,5 @@ class Song
       nil
     end
   end
+
 end
