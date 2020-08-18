6c73ac3d65a3b83f4d3cf4f03e89bec1d678a1d8
Cory Marker <cmarker4@gmail.com>
2020-08-18 17:17:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,10 +19,10 @@ class Artist
   end
 
   def add_song_by_name(song_name)
+    @@song_count += 1
     song = Song.new(song_name)
     @songs << song
     song.artist = self
-    @@song_count += 1
   end
 
   def self.song_count
