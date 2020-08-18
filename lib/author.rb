97df3cbd166a7d05315a658fc0e96d62adf6b4b2
Cory Marker <cmarker4@gmail.com>
2020-08-18 17:23:08 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author
   attr_accessor :name, :posts
 
-  @@post_count = 0
+  @@post_count = 1
 
   def initialize(name)
     @name = name
@@ -15,7 +15,7 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
-    # @@post_count += 1
+    @@post_count += 1
   end
 
   def add_post_by_title(post_title)
