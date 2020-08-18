dcc75fe3bbc8883ceb6dddfd6b5d618e26f09fa0
Cory Marker <cmarker4@gmail.com>
2020-08-18 17:21:53 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -16,14 +16,14 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
-    @@post_count += 1
+    # @@post_count += 1
   end
 
   def add_post_by_title(post_title)
     post = Post.new(post_title)
     @posts << post
     post.author = self
-    @@post_count += 1
+    # @@post_count += 1
   end
 
   def self.post_count
